33ff403be6f350ab70a43e76398e814a4dd79fc1
Catalina <capalvarez.i@gmail.com>
2017-08-31 22:40:03 -0300

    Adds documentation

--- test/ReadPointsFromFileTestMain.cpp
+++ test/ReadPointsFromFileTestMain.cpp
@@ -7,7 +7,7 @@ int main(){
     std::vector<Point> square_points = {Point(0,0), Point(1,0), Point(1,1), Point(0,1)};
     Region square(square_points);
 
-    square.addSeedsFromFile("points_file.txt");
+    square.addSeedsFromFile("/Software/Delynoi/test/test_examples/points_file.txt");
     std::vector<Point> seeds = square.getSeedPoints();
 
     TriangleMeshGenerator generator (seeds, square);
